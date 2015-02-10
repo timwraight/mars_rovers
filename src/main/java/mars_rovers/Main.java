3d34fe84779704d6e89779281edcb766438b34e1
Tim Wraight <tim@wraight.net>
2015-02-10 08:08:38 +0000

    Initial commit

---
+++ src/main/java/mars_rovers/Main.java
@@ -0,0 +1,16 @@
+package mars_rovers;
+
+import com.google.common.base.Strings;
+
+public class Main {
+
+    public static void main(String[] args) {
+          
+    }
+
+    static String triple(String str) {
+        return Strings.repeat(str, 3);
+    }
+
+
+}
---
+++ src/test/java/mars_rovers/MainTest.java
@@ -0,0 +1,12 @@
+package mars_rovers;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void testTriple() {
+        assertThat(Main.triple("AB"), equalTo("ABABAB"));
+    }
+}
